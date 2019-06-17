80cb24e2a7f0d153da42d3de5774f049652f3758
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-18 00:37:37 +0800

    +

--- GB_Arch+Patterns_Brizhak_HW06_01/Program.cs
+++ GB_Arch+Patterns_Brizhak_HW06_01/Program.cs
@@ -26,6 +26,29 @@ namespace GB_Arch_Patterns_Brizhak_HW06_01
             firstElement.LinkElement(secondElement);
             secondElement.LinkElement(thirdElement);
 
+            decimal r0 = 0;
+            decimal r10 = 10;
+            decimal r88 = 88;
+            decimal r101 = 101;
+
+            Console.WriteLine($"Запрос {r0} ");
+            firstElement.Сonsent(r0);
+            Console.ReadKey();
+
+            Console.WriteLine($"Запрос {r10} ");
+            firstElement.Сonsent(r10);
+            Console.ReadKey();
+
+            Console.WriteLine($"Запрос {r88} ");
+            firstElement.Сonsent(r88);
+            Console.ReadKey();
+
+            Console.WriteLine($"Запрос {r101} ");
+            firstElement.Сonsent(r101);
+            Console.ReadKey();
+
+
+
         }
     }
 }
