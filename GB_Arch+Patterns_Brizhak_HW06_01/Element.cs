04fd6537952d28c973bfdff69b0182d072e59003
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-17 22:49:50 +0800

    +

---
+++ GB_Arch+Patterns_Brizhak_HW06_01/Element.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW06_01
+{
+    class Element : IElement
+    {
+        private IElement _next;
+
+        public Element()
+        {
+        }
+
+        Employee IElement.Employee { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        void IElement.LinkElement(IElement next)
+        {
+            this._next = next;
+        }
+    }
+}
