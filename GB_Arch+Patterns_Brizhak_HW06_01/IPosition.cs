78c05a3f74d3b1fa5749c676d732bfa83dd4a9e2
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-17 21:13:20 +0800

    + interface IPosition +public  interface IEmployee

---
+++ GB_Arch+Patterns_Brizhak_HW06_01/IPosition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW06_01
+{
+    interface IPosition
+    {
+        string Position { get; set; }
+        decimal Limit { get; set; }
+    }
+}
