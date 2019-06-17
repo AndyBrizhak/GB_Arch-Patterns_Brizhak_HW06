2bc26aaaeede7b25f4dc3d3e185e27e3589fdb36
AndyBrizhak <andrewbrizhak@gmail.com>
2019-06-17 21:43:42 +0800

    +public class Employee : IEmployee, IPosition

---
+++ GB_Arch+Patterns_Brizhak_HW06_01/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB_Arch_Patterns_Brizhak_HW06_01
+{
+    public class Employee : IEmployee, IPosition
+    {
+        public string Name { get; set; }
+        public string Position { get; set ; }
+        public decimal Limit { get; set; }
+        
+    }
+}
